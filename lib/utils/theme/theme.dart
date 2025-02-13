286fdb9c96d03ddc51138a4d2d02586f4485f87c
KimsunLY <163507070+Kimsunly@users.noreply.github.com>
2025-02-13 14:31:27 +0700

    Theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -13,7 +13,7 @@ class TAppTheme{
   TAppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: GAAYYYYYYYYY,
+    useMaterial3: false,
     fontFamily: 'Poppins',
     brightness: Brightness.light,
     primaryColor: Colors.blue,
