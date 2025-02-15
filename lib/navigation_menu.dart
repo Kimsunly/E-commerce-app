1ab5d5c99926a3f531ced6f7b68e5af959144bc8
Kimchhay108 <thoeurn.kimchhay.2823@rupp.edu.kh>
2025-02-15 23:14:37 +0700

    Navigation new

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-class  extends StatelessWidget {
-  const ({super.key});
+
+
+class  NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-    bottomNavigationBar:
+    return  Scaffold(
+     bottomNavigationBar: NavigationBar(
+      destinations: [
+        const NavigationDestination(icon: icon(Iconsax.home), label: 'Home      ')
+        Container(color: Colors.green,),
+        Container(color: Colors.cyan,),
+        Container(color: Colors.deepPurple,),
+        Container(color: Colors.orange),
+      ],
+     ),
     );
   }
 }
