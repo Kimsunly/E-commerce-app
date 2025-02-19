3864eccbf9886fd633cea96a3d26c6ec1c487d14
KimsunLY <163507070+Kimsunly@users.noreply.github.com>
2025-02-19 14:57:50 +0700

    Splash and utils upadate

---
+++ lib/utils/logging/logger.dart
@@ -0,0 +1,34 @@
+import 'package:logger/logger.dart';
+
+class TLoggerHelper{
+  static final Logger _logger = Logger(
+    printer : PrettyPrinter(),
+    // we can customize the log levels here based on our needs
+    level: Level.debug,
+  );
+
+  static void debug(String message){
+    _logger.d(message);
+  }
+
+  static void info(String message){
+    _logger.i(message);
+  }
+
+  static void warning(String message){
+    _logger.w(message);
+  }
+
+  static void error(String message, [dynamic error]){
+    _logger.e(message, error: error, stackTrace: StackTrace.current);
+  }
+
+
+
+
+
+
+
+
+
+}
