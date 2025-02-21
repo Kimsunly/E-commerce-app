b3ae6504b916c0a2142913f6d974e39cab25c62b
KimsunLY <163507070+Kimsunly@users.noreply.github.com>
2025-02-22 00:41:34 +0700

    Splash and utils upadate

---
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_next.dart
@@ -0,0 +1,29 @@
+import 'package:e_commerce_app/features/authentication/controllers.onboarding/onboarding.controller.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/device/device_utility.dart';
+import '../../../../../utils/helpers/helper_functions.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  static var instance;
+
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight()+25,
+      child: ElevatedButton(
+        onPressed: () => OnBoardingController.instance.nextPage(),
+        style: ElevatedButton.styleFrom(shape: const CircleBorder(), backgroundColor: dark ? TColors.primary : Colors.black),
+        child: Icon(Iconsax.arrow_right_3),
+      ),
+    );
+  }
+}
