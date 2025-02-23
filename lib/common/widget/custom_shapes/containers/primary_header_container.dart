3e8cd4e76d8cc4d4eac729289d9ae8a637edd921
Kimchhay108 <thoeurn.kimchhay.2823@rupp.edu.kh>
2025-02-23 14:49:58 +0700

    changed colors.dart

---
+++ lib/common/widget/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,43 @@
+import 'package:e_commerce_app/common/widget/custom_shapes/curved_edges/curved_edge_widget.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+import 'circular_container.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.only(bottom: 0),
+        child: Stack(
+          children: [
+            Positioned(
+              top: -100,
+              right: -250,
+              child: TCircularContainer(
+                backgroundColor: TColors.textWhite.withOpacity(0.1),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              right: -300,
+              child: TCircularContainer(
+                backgroundColor: TColors.textWhite.withOpacity(0.1),
+              ),
+            ),
+            child,
+          ],
+        ),
+      ),
+    );
+  }
+}
