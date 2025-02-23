2f265a641227b557ccef10177268fea57a6ef3df
Kimchhay108 <thoeurn.kimchhay.2823@rupp.edu.kh>
2025-02-23 12:32:29 +0700

    lol

--- lib/common/widget/custom_shapes/curved_edges/curved_edges.dart
+++ lib/common/widget/custom_shapes/curved_edges/curved_edges.dart
@@ -3,5 +3,13 @@ import 'package:flutter/material.dart';
 class TcustomCurvedEdges extends CustomClipper<Path>{
   @overide
   Path getClip(Size size){
+    var path = Path();
+    path.lineTo(0, size.height)
  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    throw UnimplementedError();
+  }
 }
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -47,3 +47,6 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+class CustomHeaderClipper {
+}
+
