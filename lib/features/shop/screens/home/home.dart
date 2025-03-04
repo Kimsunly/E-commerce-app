8c24f7160f6436ccde1307b0726ad5d562b56217
Kimchhay108 <thoeurn.kimchhay.2823@rupp.edu.kh>
2025-03-04 23:47:39 +0700

    Search Container in progress

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,11 +1,15 @@
 import 'package:e_commerce_app/common/widget/appbar/appbar.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widget/home_appbar.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/device/device_utility.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import '../../../../common/widget/custom_shapes/containers/circular_container.dart';
+import '../../../../common/widget/custom_shapes/containers/search_container.dart';
 import '../../../../common/widget/custom_shapes/curved_edges/curved_edge_widget.dart';
 import '../../../../common/widget/products_cart/cart_menu_icon.dart';
+import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/constants/text_strings.dart';
 import 'package:e_commerce_app/common/widget/products_cart/cart_menu_icon.dart';
 
@@ -21,14 +25,26 @@ class HomeScreen extends StatelessWidget {
           // Custom clipped background section
           TPrimaryHeaderContainer(
             child: Column(
-              children: [],
+              children: [
+
+              ],
             ),
           ),
           // AppBar on top of the clipped section
           THomeAppbar(),
-          // Other widgets below can go here
+          SizedBox(height: TSizes.spaceBtwSections),
+          //search bar
+          TSearchContainer( text: 'Search in FUCKERS'),
+          SizedBox(height: TSizes.spaceBtwSections),
+          //CATERGORIES
+          Padding(padding: EdgeInsets.only(left: TSizes.defaultSpace), child: Column(children: [
+            Text('Popular Categories', style: Theme.of(context).textTheme.headlineSmall),
+          ],
+          ),
+          )
         ],
       ),
     );
   }
 }
+
